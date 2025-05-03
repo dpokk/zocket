cda337f5c7eb60597d5aa838aa0caf071db6d6bc
Deepak <deepak.reddy45516@gmail.com>
2025-05-03 13:41:39 +0530

    fix: base url update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001" // Local backend URL
+  : "https://realtime-chatapp-git-main-dpokk-projects.vercel.app/"; // Replace with your backend's production URL
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true, // Ensure cookies are sent
 });
