27a79b6cc942bb1f96b312f2ed2f9cf69f39cb85
Deepak <deepak.reddy45516@gmail.com>
2025-05-03 13:45:54 +0530

    Revert "fix: base url update"
    This reverts commit cda337f5c7eb60597d5aa838aa0caf071db6d6bc.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001" // Local backend URL
-  : "https://realtime-chatapp-git-main-dpokk-projects.vercel.app/"; // Replace with your backend's production URL
-
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, // Ensure cookies are sent
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    withCredentials: true,
 });
