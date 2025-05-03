5460604de2e9610f9f1f042002fd6283f311a3e0
Deepak <deepak.reddy45516@gmail.com>
2025-05-03 13:22:53 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/realtime-chatapp',
-  build: {
-    outDir: 'frontend/dist', // Ensure this is set to 'dist'
-  },
 })
