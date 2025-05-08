b22ec944b982a7e91c5cf2400079b1983e7a678d
Deepak <deepak.reddy45516@gmail.com>
2025-05-08 21:39:38 +0530

    Revert "fix: cross origin request error handled"
    This reverts commit 05b92e06d1a77ba36dc02dd78fd210eed4e1e5a2.

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //prevents cross site script attacks
-        sameSite: "none",
+        sameSite: "strict",
         secure: process.env.NODE_ENV !== "development",
     });
 
