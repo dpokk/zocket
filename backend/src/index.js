16dcb949516e6d8dd200a17ffd5fb61297cc9c05
Deepak <deepak.reddy45516@gmail.com>
2025-05-08 21:39:14 +0530

    Revert "fix: updated URL endpoints"
    This reverts commit e67c25b342b07d86aaf14b3c88902246edcc83b4.

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,14 +10,14 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({
-    origin: process.env.NODE_ENV === "production" ? "https://zockett.vercel.app/" : "http://localhost:5173",
+app.use(cors ({
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 
