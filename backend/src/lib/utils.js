05b92e06d1a77ba36dc02dd78fd210eed4e1e5a2
Deepak <deepak.reddy45516@gmail.com>
2025-05-08 18:28:04 +0530

    fix: cross origin request error handled

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //prevents cross site script attacks
-        sameSite: "strict",
+        sameSite: "none",
         secure: process.env.NODE_ENV !== "development",
     });
 
