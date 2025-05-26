039c02cd5fe8b3b34f3a287283ef738991dd16b8
Deepak <deepak.reddy45516@gmail.com>
2025-05-26 23:18:07 +0530

    syntax error

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,7 +3,9 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: "7d",
-    })    res.cookie("jwt", token, {
+    });
+    
+    res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //ms
         httpOnly: true, //prevents cross site script attacks
         sameSite: "none", // required for cross-origin requests
